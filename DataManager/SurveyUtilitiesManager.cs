0032e869d5326fee0230287af428b67bb25eaa91
AJ <anthonyfields1@gmail.com>
2018-03-21 12:11:46 -0400

    created branch for issue#1 & test comment

--- DataManager/SurveyUtilitiesManager.cs
+++ DataManager/SurveyUtilitiesManager.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.IO;
 using System.Text.RegularExpressions;
-
+//this is a test commit to fix1 branch
 namespace DataManager
 {
     class SurveyUtilitiesManager : IDataErrorInfo
