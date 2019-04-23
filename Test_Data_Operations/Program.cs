109b4aa83b008e712129295b7192b7a28af33935
AJ <anthonyfields1@gmail.com>
2019-04-23 11:21:32 -0400

    iss20: created Test_Data_Operations project to help test public funcitons in DataManager

---
+++ Test_Data_Operations/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataManager;
+
+namespace Test_Data_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
