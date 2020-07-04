19ab9b752ed17b52399dcf29d59fd4e316435b77
AJ <anthonyfields1@gmail.com>
2020-07-04 00:38:52 -0400

    Added skeleton of Standard page

---
+++ DMCoreGUI/Validation/StandardViewValidationRules.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace DMCoreGUI.Validation
+{
+    public class StartRowValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                var _ = Int32.Parse((string)value);
+            }
+            catch (Exception)
+            {
+                return new ValidationResult(false, "Illegal input given, try again");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+
+    public class SheetNameValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrEmpty((string)value))
+            {
+                return new ValidationResult(false, "Sheet Required!");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
--- DMCoreGUI/View/Standard.xaml.cs
+++ DMCoreGUI/View/Standard.xaml.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DMCoreGUI.ViewModel;
 
 namespace DMCoreGUI.View
 {
@@ -18,9 +10,45 @@ namespace DMCoreGUI.View
     /// </summary>
     public partial class Standard : Page
     {
+        StandardViewModel vm = new StandardViewModel();
+
+
         public Standard()
         {
             InitializeComponent();
         }
+
+        private void SlctFiles_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Run_Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Addpw_Click(object sender, RoutedEventArgs e)
+        {
+            if (pw.Text != "")
+            {
+                vm.PW_list.Add(pw.Text);
+                pw.Text = null;
+            }
+        }
+
+        private void Pwrm_Click(object sender, RoutedEventArgs e)
+        {
+            vm.PW_list.Remove(passwords.SelectedItem.ToString());
+        }
+
+        private void RmAllPW_Click(object sender, RoutedEventArgs e)
+        {
+            vm.PW_list.Clear();
+        }
+
+
     }
 }
